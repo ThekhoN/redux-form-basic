0810ba758b983360b5d9452dd68dcbd5be2dc8c5
ThekhoN <bennetonss@hotmail.com>
2017-04-16 20:01:37 +0530

    added validation

--- src/component/form.js
+++ src/component/form.js
@@ -1,6 +1,36 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 
+const validate = values => {
+  const errors = {};
+  if(!values.name){
+    errors.name = 'Required';
+  }
+  if(!values.email){
+    errors.email = 'Required';
+  }
+  if(!values.password){
+    errors.password = 'Required';
+  }
+  if(!values.passwordConfirm){
+    errors.passwordConfirm = 'Required';
+  }
+  if(values.password !== values.passwordConfirm){
+    errors.passwordConfirm = 'Passwords should match!';
+  }
+  return errors;
+};
+
+const renderField = ({input, label, type, meta: { touched, error }}) => (
+  <div className='form-field'>
+    <label>{ label }</label>
+    <div>
+      <input {...input} placeholder={label} type={type} name={label}/>
+      {touched && error && <span>{error}</span>}
+    </div>
+  </div>
+)
+
 class Form extends Component {
   render(){
     const {
@@ -11,18 +41,10 @@ class Form extends Component {
     } = this.props;
     return (
       <form onSubmit={handleSubmit}>
-          <label>
-            <span>name:</span>
-            <Field type='text' placeholder='name' name='name' component='input'/>
-          </label>
-          <label>
-            <span>email:</span>
-            <Field type='text' placeholder='email' name='email' component='input'/>
-          </label>
-          <label>
-            <span>password:</span>
-            <Field type='text' placeholder='password' name='password' component='input'/>
-          </label>
+        <Field type='text' name='name' component={renderField} label='name'/>
+        <Field type='email' name='email' component={renderField} label='email'/>
+        <Field type='password' name='password' component={renderField} label='password'/>
+        <Field type='password' name='passwordConfirm' component={renderField} label='passwordConfirm'/>
           <p>
             <button type='submit' disabled={pristine || submitting}>submit</button>
           </p>
@@ -35,5 +57,6 @@ class Form extends Component {
 }
 
 export default reduxForm({
-  form:'form'
+  form:'form',
+  validate
 })(Form);
